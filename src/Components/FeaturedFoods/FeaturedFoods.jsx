a55ff9ecdcbdfb00a9f6f633d080db222fe328f3
rahulsd380 <rahulsd380@gmail.com>
2023-11-05 18:03:09 +0600

    featured food section created

--- src/Components/FeaturedFoods/FeaturedFoods.jsx
+++ src/Components/FeaturedFoods/FeaturedFoods.jsx
@@ -4,11 +4,7 @@ import { Link } from "react-router-dom";
 const FeaturedFoods = () => {
     const [foods, setFoods] = useState([]);
 
-    useEffect( () => {
-        fetch('http://localhost:5000/featured-foods')
-        .then(res => res.json())
-        .then(data => setFoods(data))
-    }, [])
+
     return (
         <div>
             <div className="grid px-5 md:p-0 md:grid-cols-2 gap-5 max-w-7xl mx-auto mb-4 mt-10">
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,10 +1,12 @@
 import SimpleSlider from "../Banner/Banner";
+import FeaturedFoods from "../FeaturedFoods/FeaturedFoods";
 import Navbar from "../Navbar/Navbar";
 
 
 const Home = () => {
     return (
         <div>
+            <FeaturedFoods></FeaturedFoods>
             <Navbar></Navbar>
             <SimpleSlider></SimpleSlider>
         </div>
