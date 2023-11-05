884367380aa561cb04632070bc5c8ef65e61a706
rahulsd380 <rahulsd380@gmail.com>
2023-11-05 18:02:19 +0600

    googlr signin implemented

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,12 +1,10 @@
 import SimpleSlider from "../Banner/Banner";
-import FeaturedFoods from "../FeaturedFoods/FeaturedFoods";
 import Navbar from "../Navbar/Navbar";
 
 
 const Home = () => {
     return (
         <div>
-            <FeaturedFoods></FeaturedFoods>
             <Navbar></Navbar>
             <SimpleSlider></SimpleSlider>
         </div>
