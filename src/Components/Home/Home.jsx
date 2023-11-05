e80ee2c415b3484f3e3d0240569943ecf4b10351
rahulsd380 <rahulsd380@gmail.com>
2023-11-05 18:01:42 +0600

    login implemented

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,13 +1,11 @@
 import SimpleSlider from "../Banner/Banner";
 import FeaturedFoods from "../FeaturedFoods/FeaturedFoods";
 import Navbar from "../Navbar/Navbar";
-import NewCompo from "../new";
 
 
 const Home = () => {
     return (
         <div>
-            <NewCompo></NewCompo>
             <FeaturedFoods></FeaturedFoods>
             <Navbar></Navbar>
             <SimpleSlider></SimpleSlider>
